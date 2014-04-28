d8a4a8f6ff8568c8457784f7c7752c41beaa90a8
Tobias Moebert <tobias.moebert@me.com>
2014-04-28 16:54:10 +0200

    + added basic documentation to FactOperator enum

--- src/main/java/de/unipotsdam/rulegenerator/enums/FactOperator.java
+++ src/main/java/de/unipotsdam/rulegenerator/enums/FactOperator.java
@@ -1,5 +1,30 @@
 package de.unipotsdam.rulegenerator.enums;
 
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Enum FactOperator.
+ */
 public enum FactOperator {
-	IS, IS_NOT, GREATER_THEN, LESS_THEN, DEVIDE, MULTIPLY, SUBTRACT, SUM, NO_VALUE, COMPLETE
+
+	/** The is. */
+	IS,
+	/** The is not. */
+	IS_NOT,
+	/** The greater then. */
+	GREATER_THEN,
+	/** The less then. */
+	LESS_THEN,
+	/** The devide. */
+	DEVIDE,
+	/** The multiply. */
+	MULTIPLY,
+	/** The subtract. */
+	SUBTRACT,
+	/** The sum. */
+	SUM,
+	/** The no value. */
+	NO_VALUE,
+	/** The complete. */
+	COMPLETE;
 }
