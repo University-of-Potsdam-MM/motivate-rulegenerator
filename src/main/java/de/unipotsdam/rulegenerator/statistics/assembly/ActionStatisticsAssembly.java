1e2fa88bcadeb8de82c2dcd4ee7ed8ebba1feaec
Helena Jank <jank@uni-potsdam.de>
2015-07-24 14:32:21 +0200

    + added workspace.xml to .gitignore # reduced CancelActionStatisticsAssembly by moving code from generateStatistics() into super function collectReasonsForGivenAction()

--- src/main/java/de/unipotsdam/rulegenerator/statistics/assembly/ActionStatisticsAssembly.java
+++ src/main/java/de/unipotsdam/rulegenerator/statistics/assembly/ActionStatisticsAssembly.java
@@ -2,8 +2,18 @@ package de.unipotsdam.rulegenerator.statistics.assembly;
 
 import java.util.Collection;
 
+import com.clarkparsia.pellet.owlapiv3.PelletReasoner;
+import com.clarkparsia.pellet.owlapiv3.PelletReasonerFactory;
+import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.InfModel;
 import com.hp.hpl.jena.rdf.model.Literal;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.RDFNode;
+import de.unipotsdam.rulegenerator.ontology.CancelAction;
+import de.unipotsdam.rulegenerator.statistics.Reason;
+import de.unipotsdam.rulegenerator.statistics.StatisticsList;
+import org.mindswap.pellet.KnowledgeBase;
+import org.mindswap.pellet.jena.PelletInfGraph;
 import org.semanticweb.owlapi.model.OWLOntology;
 
 import com.hp.hpl.jena.vocabulary.OWL;
@@ -21,10 +31,24 @@ public abstract class ActionStatisticsAssembly extends StatisticsAssembly {
 	protected RDFNode user, recContext, lu, metaDataProp;
 	protected Literal recTime, actTime, metaDataValue;
 
+	public ActionStatisticsAssembly(OWLOntology ontology) {
+		this.ontology = ontology;
+		MyFactory factory = new MyFactory(ontology);
+		this.learningUnits = factory.getAllLearningUnitInstances();
+	}
+
+	public Collection<? extends LearningUnit> getLearningUnits() {
+		return learningUnits;
+	}
+
+	public void setLearningUnits(
+			Collection<? extends MyLearningUnit> learningUnits) {
+		this.learningUnits = learningUnits;
+	}
+
 	protected String getFirstQuery() throws Exception {
 		if (action == null)
 			throw new Exception("Action is not defined.");
-
 		return ""
 				+ "PREFIX kno: <http://motivate-project.de/ontology/knowledge.owl#> "
 				+ "PREFIX rdfs: <"
@@ -63,7 +87,6 @@ public abstract class ActionStatisticsAssembly extends StatisticsAssembly {
 			throw new Exception("RecTime is not defined.");
 		if (actTime == null)
 			throw new Exception("ActTime is not defined.");
-
 		return ""
 				+ "PREFIX kno: <http://motivate-project.de/ontology/knowledge.owl#> "
 				+ "PREFIX rdfs: <"
@@ -97,18 +120,69 @@ public abstract class ActionStatisticsAssembly extends StatisticsAssembly {
 				;
 	}
 
-	public ActionStatisticsAssembly(OWLOntology ontology) {
-		this.ontology = ontology;
-		MyFactory factory = new MyFactory(ontology);
-		this.learningUnits = factory.getAllLearningUnitInstances();
-	}
+	protected void collectReasonsForGivenAction() {
 
-	public Collection<? extends LearningUnit> getLearningUnits() {
-		return learningUnits;
-	}
+		PelletReasoner reasoner = PelletReasonerFactory.getInstance()
+				.createNonBufferingReasoner(ontology);
 
-	public void setLearningUnits(
-			Collection<? extends MyLearningUnit> learningUnits) {
-		this.learningUnits = learningUnits;
+		// Get the KB from the reasoner
+		KnowledgeBase kb = reasoner.getKB();
+		// Create a Pellet graph using the KB from OWLAPI
+		PelletInfGraph graph = new org.mindswap.pellet.jena.PelletReasoner()
+				.bind(kb);
+		// Wrap the graph in a model
+		InfModel model = ModelFactory.createInfModel(graph);
+		// Use the model to answer SPARQL queries
+
+		String firstQuery = null;
+		String secondQuery = null;
+
+		try {
+			firstQuery = getFirstQuery();
+
+			//"select * where {"+action+" ?x ?z. ?a ?b "+action+". }"
+			Query fquery = QueryFactory.create(firstQuery);
+			QueryExecution fqe = QueryExecutionFactory.create(fquery, model);
+			ResultSet results = fqe.execSelect();
+			//ResultSetFormatter.out(System.out, results, fquery);
+
+			while(results.hasNext()) {
+				QuerySolution row = results.next();
+				user = row.get("user");
+				actTime = row.getLiteral("actTime");
+				recTime = row.getLiteral("recTime");
+
+				secondQuery = getSecondQuery();
+				Query squery = QueryFactory.create(secondQuery);
+				QueryExecution sqe = QueryExecutionFactory.create(squery, model);
+				ResultSet finalResults = sqe.execSelect();
+				//ResultSetFormatter.out(System.out, finalResults, squery);
+
+				while (finalResults.hasNext()) {
+					QuerySolution srow = finalResults.next();
+					lu = srow.get("lu");
+					recContext = srow.get("recContext");
+					metaDataProp = srow.get("metaDataProp");
+					metaDataValue = srow.getLiteral("metaDataValue");
+
+					Reason reason = new Reason();
+					reason.setAction(action);
+					reason.setActionTime(actTime);
+					reason.setUser(user);
+					reason.setRecordedTime(recTime);
+					reason.setLearningUnit(lu);
+					reason.setRecordedContextInformation(recContext);
+					reason.setMetaDataProperty(metaDataProp);
+					reason.setMetaDataValue(metaDataValue);
+
+					reasons.addReason(reason);
+				}
+				sqe.close();
+			}
+			fqe.close();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
--- src/main/java/de/unipotsdam/rulegenerator/statistics/assembly/CancelActionStatisticsAssembly.java
+++ src/main/java/de/unipotsdam/rulegenerator/statistics/assembly/CancelActionStatisticsAssembly.java
@@ -30,71 +30,10 @@ public class CancelActionStatisticsAssembly extends ActionStatisticsAssembly {
 					.getCancelActions();
 
 			for (CancelAction cancelAction : cancelActions) {
-				PelletReasoner reasoner = PelletReasonerFactory.getInstance()
-						.createNonBufferingReasoner(ontology);
-				// Get the KB from the reasoner
-				KnowledgeBase kb = reasoner.getKB();
-				// Create a Pellet graph using the KB from OWLAPI
-				PelletInfGraph graph = new org.mindswap.pellet.jena.PelletReasoner()
-						.bind(kb);
-				// Wrap the graph in a model
-				InfModel model = ModelFactory.createInfModel(graph);
-				// Use the model to answer SPARQL queries
-
 				action = cancelAction.getOwlIndividual().getIRI().toQuotedString();
-				String firstQuery = null;
-				String secondQuery = null;
-
-				try {
-					firstQuery = getFirstQuery();
-
-					//"select * where {"+action+" ?x ?z. ?a ?b "+action+". }"
-					Query fquery = QueryFactory.create(firstQuery);
-					QueryExecution fqe = QueryExecutionFactory.create(fquery, model);
-					ResultSet results = fqe.execSelect();
-					//ResultSetFormatter.out(System.out, results, fquery);
-
-					while(results.hasNext()) {
-						QuerySolution row = results.next();
-						user = row.get("user");
-						actTime = row.getLiteral("actTime");
-						recTime = row.getLiteral("recTime");
-
-						secondQuery = getSecondQuery();
-						Query squery = QueryFactory.create(secondQuery);
-						QueryExecution sqe = QueryExecutionFactory.create(squery, model);
-						ResultSet finalResults = sqe.execSelect();
-						//ResultSetFormatter.out(System.out, finalResults, squery);
-
-						while (finalResults.hasNext()) {
-							QuerySolution srow = finalResults.next();
-							lu = srow.get("lu");
-							recContext = srow.get("recContext");
-							metaDataProp = srow.get("metaDataProp");
-							metaDataValue = srow.getLiteral("metaDataValue");
-
-							Reason reason = new Reason();
-							reason.setAction(action);
-							reason.setActionTime(actTime);
-							reason.setUser(user);
-							reason.setRecordedTime(recTime);
-							reason.setLearningUnit(lu);
-							reason.setRecordedContextInformation(recContext);
-							reason.setMetaDataProperty(metaDataProp);
-							reason.setMetaDataValue(metaDataValue);
-
-							reasons.addReason(reason);
-						}
-						sqe.close();
-					}
-					fqe.close();
-
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
+				collectReasonsForGivenAction();
 			}
 		}
-
 		return reasons;
 	}
 }
